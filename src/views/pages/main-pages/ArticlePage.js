9cba7d23ec16e3c69faaa212625c5c31e5c6eab6
lewisseo91 <togokr@naver.com>
2021-09-25 00:34:39 +0900

    style : alignment

--- src/views/pages/main-pages/ArticlePage.js
+++ src/views/pages/main-pages/ArticlePage.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import ArticleBoard from './ArticleBoard';
 import ArticleRegistration from './ArticleRegistration';
